9765f58baa7a236200daaf563f1c00660e679914
basdxz <tudurap.com@gmail.com>
2022-03-18 19:05:59 +0000

    Absolute pog I can imagine, can't be without it!

---
+++ src/main/java/com/basdxz/vshadeloadingscreen/VShadeLoadingScreen.java
@@ -0,0 +1,8 @@
+package com.basdxz.vshadeloadingscreen;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
+public class VShadeLoadingScreen {
+
+}
